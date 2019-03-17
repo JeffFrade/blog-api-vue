80a2159db867b18442c7b816b8f578c5a6d3eafa
Jefferson Frade <jefferrson.frade@gmail.com>
2019-03-17 02:40:47 -0300

    Installed packages

--- src/main.js
+++ src/main.js
@@ -1,15 +1,40 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import { sync } from 'vuex-router-sync'
 import Vue from 'vue'
+import Vuex from 'vuex'
+import VueResource from 'vue-resource'
+import VueRouter from 'vue-router'
 import App from './App'
-import router from './router'
+import routes from './router'
+import VuexStore from './vuex/store'
+
+Vue.use(Vuex)
+Vue.use(VueResource)
+Vue.use(VueRouter)
+
+Vue.http.options.root = process.env.SERVER
 
 Vue.config.productionTip = false
+
+const store = new Vuex.Store(VuexStore)
+const router = new VueRouter({
+  routes
+})
+
+sync(store, router)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
+
+  watch: {
+    '$route' (to, from) {
+      this.$store.dispatch('clearRegistries')
+    }
+  },
   components: { App },
   template: '<App/>'
 })
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,10 @@
-import Vue from 'vue'
-import Router from 'vue-router'
+
 import HelloWorld from '@/components/HelloWorld'
 
-Vue.use(Router)
+const routes = [{
+  path: '/',
+  name: 'HelloWorld',
+  component: HelloWorld
+}]
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
-})
+export default routes
